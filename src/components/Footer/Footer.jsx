23789c5063915a990de87af18490fcb71990239a
i-Lene <marlene.s.veiga@gmail.com>
2025-04-30 15:03:25 +0100

    footer done, homepage in progress

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.scss";
+
+function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <p>Copyright © 2025 Designed and Developed by Marlene Sebastião Veiga</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <div className="home__description">
+        <p>Hello there!</p>
+        <h1>I'm Marlene</h1>
+        <p className="jobTitle">Frontend Developer</p>
+      </div>
+      <div className="home__image">IMAGE</div>
+    </div>
+  );
 }
 
 export default Home;
