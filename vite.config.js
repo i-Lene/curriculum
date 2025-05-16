997609264018505991d614b5563454d00c9208d4
i-Lene <marlene.s.veiga@gmail.com>
2025-05-16 13:21:46 +0100

    done v6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: '/curriculum/',
   plugins: [react()],
-})
+  base: '/curriculum/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
+});
