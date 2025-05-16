d3860a09e0985aca1c3fde751e9379b69e007df1
i-Lene <marlene.s.veiga@gmail.com>
2025-05-16 13:49:45 +0100

    done v11

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/curriculum/',
+  base: '/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      }
-    }
-  }
+  },
 });
