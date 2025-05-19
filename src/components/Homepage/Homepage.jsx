dc46623ddadd20f067023974ea863e0131d0924f
i-Lene <marlene.s.veiga@gmail.com>
2025-05-19 17:29:29 +0100

    fixes 6

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,6 +3,7 @@ import meCartoon from "/images/mecartoon.svg";
 import classes from "./Homepage.module.scss";
 import CustomButton from "../UI/CustomButton";
 import { motion } from "motion/react";
+import arrows from "/images/arrows.gif";
 
 function Homepage() {
   function handleClick(e) {
@@ -24,15 +25,24 @@ function Homepage() {
         </div>
       </div>
       <div className={classes.home__about}>
+        <span>
+          <img className={classes.arrows} src={arrows} alt="arrows" />
+        </span>
         <div className={classes.home__about__description}>
           <h2 className={classes.home__about__title}>
             Let me introduce myself !
           </h2>
-          <p className={classes.home__about__text}>
-            I am a passionate frontend developer with an ambition for creating
-            beautiful and functional user interfaces. I love to learn new
-            technologies and improve my skills.
-          </p>
+          <div className={classes.home__about__text}>
+            <p>
+              Hello! I'm <strong>Marlene</strong>, and this is my portfolio
+              where you can get to know me and my professional background.
+            </p>
+            <p>
+              I'm a <strong>frontend developer</strong>, ambitious, curious and
+              always ready to learn more. I enjoy working in a team and I
+              consider myself a good communicator.
+            </p>
+          </div>
           <p className={classes.home__about__text}>
             I am currently looking for new opportunities to grow and make an
             impact in the tech world.
@@ -40,6 +50,10 @@ function Homepage() {
           <p className={classes.home__about__text}>
             Feel free to reach out to me if you have any questions or just want
             to chat!
+          </p>
+          <p className={classes.home__about__text}>
+            <strong>P.S:</strong> This website is mobile ready, check me out on
+            your phone!
           </p>
         </div>
         <div className={classes.home__callToAction}>
